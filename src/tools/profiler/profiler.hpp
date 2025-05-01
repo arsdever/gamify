2d8b0321c7cfd3d18f35aa81aca962dc0a0bbeaf
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2025-05-01 10:49:55 +0400

    Refactor profiler class
    Replace logic of rendering with the implemented classes
    
    Replace the usage of renderer classes with the profiler in the
    profiler widget

--- src/tools/profiler/profiler.hpp
+++ src/tools/profiler/profiler.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "core/window.hpp"
+#include "prof/data.hpp"
 
 class profiler : public core::window
 {
 public:
+    using element_type = std::variant<prof::frame, prof::data_sample>;
+
+public:
     profiler();
     ~profiler();
+
+    void set_frame(prof::frame f);
+    void unset_frame();
+
+    glm::vec2 zoom();
+    void set_zoom(glm::vec2 z);
+
+    glm::vec2 scroll();
+    void scroll_to(glm::vec2 s);
+
+    std::optional<element_type> get_at(const glm::uvec2& pos) const;
 
 protected:
     void initialize();
